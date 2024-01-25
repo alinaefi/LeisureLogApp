8e65e8ea2d88d66ea402bd1b8891c3725b9723f2
Alina Efimova <efimovaalina2011@gmail.com>
2024-01-25 11:38:24 +0000

    env setup, backend initial setup, post model, post serializer

---
+++ backend/leisure_log_app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Post(models.Model):
+    CONTENT_TYPES = (
+        ('mv', 'movie'),
+        ('msc', 'music'),
+        ('act', 'activity'),
+        ('vgm', 'video game'),
+        ('bgm', 'board game'),
+        ('loc', 'place'),
+        ('o', 'other')
+    )
+    RATING = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5')
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
+    url = models.URLField(blank=True, default='')
+    type = models.CharField(max_length=3, choices = CONTENT_TYPES, blank=False)
+    comments = models.TextField()
+    rating = models.IntegerField(choices=RATING, blank=False)
+
+    class Meta:
+        ordering = ['created']
+    def _str_(self):
+        return self.title
+    
+
+
+
