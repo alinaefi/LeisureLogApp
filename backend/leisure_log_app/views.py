580b4f00cb6a23711a0babd69cc22d734376e522
Alina Efimova <efimovaalina2011@gmail.com>
2024-01-26 17:16:15 +0000

    created views, tested

--- backend/leisure_log_app/views.py
+++ backend/leisure_log_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from leisure_log_app.models import Post
+from leisure_log_app.serializers import PostSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    """Handles GET request to return list of posts
+    and POST request to create a new post"""
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """Handles GET request to return a specific post,
+    PUT request to update a specific post,
+    and DELETE request to delete a specific post"""
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
