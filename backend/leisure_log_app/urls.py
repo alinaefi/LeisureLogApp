8e8c8683f8810a63182ea8c605b72acae308a7f8
Alina Efimova <efimovaalina2011@gmail.com>
2024-01-29 18:17:23 +0000

    added retrospective summary view, permissions updated

--- backend/leisure_log_app/urls.py
+++ backend/leisure_log_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('posts/<int:pk>/', views.PostDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('retro/<int:months>/', views.RetroList.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/leisure_log_app/views.py
+++ backend/leisure_log_app/views.py
@@ -4,14 +4,15 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from leisure_log_app.permissions import IsAuthor
-
+from datetime import timedelta
+from django.utils import timezone
 
 class PostList(generics.ListCreateAPIView):
     """Handles GET request to return list of posts
     and POST request to create a new post"""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsAuthor]
 
     def get_queryset(self):
         """Override queryset function: only user's posts returned"""
@@ -43,3 +44,18 @@ class UserDetail(generics.RetrieveAPIView):
     and DELETE request to delete a specific user details"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class RetroList(generics.ListAPIView):
+    """Handles GET request to return list of most highly rated posts
+    for a given period of time"""
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthor]
+    
+    def get_queryset(self):
+        """Override queryset function: only user's most rated posts returned"""
+        param = self.request.query_params.get('months', 3)
+        user = self.request.user
+        return user.posts.filter(created__gt=timezone.now()-timedelta(weeks=param*4.4),
+                                rating__gt=3).order_by('type', '-created')
