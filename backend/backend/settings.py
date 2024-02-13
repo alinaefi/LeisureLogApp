5b552af6209d2c12cdbc6027b34b67dc86902cde
Alina Efimova <92220280+alinaefi@users.noreply.github.com>
2024-02-13 09:39:45 +0000

    authentication done, frontend project set up (#3)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
@@ -126,5 +127,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
--- backend/leisure_log_app/views.py
+++ backend/leisure_log_app/views.py
@@ -1,22 +1,69 @@
 from leisure_log_app.models import Post
-from leisure_log_app.serializers import PostSerializer, UserSerializer
-from rest_framework import generics
+from leisure_log_app.serializers import PostSerializer, UserSerializer, LoginSerializer, RegistrationSerializer
+from rest_framework import generics, views
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from leisure_log_app.permissions import IsAuthor
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from datetime import timedelta
 from django.utils import timezone
+from django.contrib.auth import login, logout
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
+
+class APIRootView(generics.GenericAPIView):
+    """Handles GET request to return list of api's
+    To be restricted later"""
+    def get(self, request, format=None):
+        return Response({
         'users': reverse('user-list', request=request, format=format),
         'posts': reverse('post-list', request=request, format=format),
         'retro': reverse('retro-list', request=request, format=format)
     })
+
+class LoginView(generics.GenericAPIView):
+    """Handles GET request to return login page view
+     and POST request to authenticate user"""
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = LoginSerializer
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data,
+            context={ 'request': request })
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+
+        # Redirect logged in user to account details page
+        return Response(reverse('user-detail', args=[user.id], request=request, format=format))
+
+
+class LogoutView(views.APIView):
+    """Handles GET request to log user out"""
+    def get(self, request):
+        logout(request)
+        return Response(data='logged out', status=200)
+
+
+class RegistrationView(generics.GenericAPIView):
+    """Handles GET request to return registration page view
+    and POST request to create a new user"""
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegistrationSerializer
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data,
+            context={ 'request': request })
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        # Log new user in
+        login(request, user)
+
+        # Redirect logged in user to account details page
+        return Response(reverse('user-detail', args=[user.id], request=request, format=format))
+            
 
 class PostList(generics.ListCreateAPIView):
     """Handles GET request to return list of posts
@@ -34,6 +81,7 @@ class PostList(generics.ListCreateAPIView):
         """Override saving a new object function: can only be created by current user"""
         serializer.save(author=self.request.user)
 
+
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """Handles GET request to return a specific post,
     PUT request to update a specific post,
@@ -43,11 +91,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated,
                       IsAuthor]
 
+
 class UserList(generics.ListAPIView):
     """Handles GET request to return list of users
     and POST request to create a new user"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class UserDetail(generics.RetrieveAPIView):
     """Handles GET request to return a specific user details,
@@ -55,6 +105,7 @@ class UserDetail(generics.RetrieveAPIView):
     and DELETE request to delete a specific user details"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class RetroList(generics.ListAPIView):
     """Handles GET request to return list of most highly rated posts of all time"""
@@ -66,6 +117,7 @@ class RetroList(generics.ListAPIView):
         """Override queryset function: only user's most rated posts returned"""
         user = self.request.user
         return user.posts.filter(rating__gt=3).order_by('type', '-created')
+
 
 class RetroDetail(generics.ListAPIView):
     """Handles GET request to return list of most highly rated posts
