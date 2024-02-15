865d4c394d70cb21d03b22b231d0530ed17703eb
Alina Efimova <efimovaalina2011@gmail.com>
2024-02-15 12:10:28 +0000

    non-authorized user interface done

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,7 +1,14 @@
-// LoginForm.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../App.css';
+import {
+  Input,
+  Form,
+  FormGroup,
+  Label,
+  Col,
+  Button
+} from "reactstrap";
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
@@ -11,33 +18,51 @@ const LoginForm = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('login/', { username, password });
+      const response = await axios.post('login/', { username, password }, { withCredentials: true });
       // Handle successful login
       console.log(response.data);
-    } catch (error) {
+      if (response.status === 200) {
+        alert('!LOGGED');
+      }
       // Handle login error
+    } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        className="inputBox"
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        className="inputBox"
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button className='button' type="submit">Login</button>
-    </form>
+    <Form onSubmit={handleLogin}>
+      <h1 className='my-sub-heading'>
+        Log In
+      </h1>
+      <FormGroup row>
+        <Label for="Email" sm={2}> Email: </Label>
+        <Col sm={10}>
+          <Input
+            id="Email"
+            name="email"
+            placeholder="Email"
+            type="email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="Password" sm={2}> Password: </Label>
+        <Col sm={10}>
+          <Input
+            id="Password"
+            name="password"
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Col>
+      </FormGroup>
+      <Button color="info" type="submit"> Login </Button>
+    </Form>
   );
 };
 
